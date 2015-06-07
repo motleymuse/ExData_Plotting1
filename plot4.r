0586caf52883c5275ea0d7dccae9f4ffe0f1f496
Jay Dave <jaydave09@gmail.com>
2015-06-07 18:35:39 -0500

    Fourth Commit

---
+++ plot4.r
@@ -0,0 +1,13 @@
+data <- read.table("household_power_consumption.txt", header = TRUE, stringsAsFactors=FALSE, sep=";")
+datasub <- data[data$Date %in% c("1/2/2007","2/2/2007"),]
+dtime<- as.POSIXlt(paste(as.Date(datasub$Date, format="%d/%m/%Y"), datasub$Time, sep=" "))
+par(mfrow = c(2,2))
+plot(dtime, datasub$Global_active_power, xlab=" ", ylab="Global Active Power (kilowatts)", lines(dtime, datasub$Global_active_power, type="l"), pch=20)
+plot(dtime, datasub$Voltage, xlab="datetime", ylab="Voltage", lines(dtime, datasub$Voltage, type="l"), pch=20)
+plot(dtime, datasub$Sub_metering_1, xlab=" ", ylab="Energy sub metering", lines(dtime, datasub$Sub_metering_1, type="l"), pch=20)
+lines(dtime, datasub$Sub_metering_2, type="l", col="Red")
+lines(dtime, datasub$Sub_metering_3, type="l", col="Blue")
+xrange <- range(dtime)
+yrange <- range(datasub$Sub_metering_1)
+legend("topright", c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), lty=c(1,1), col=c("black","red","blue"))
+plot(dtime, datasub$Global_reactive_power, xlab="datetime", ylab="Global_reactive_power", lines(dtime, datasub$Global_reactive_power, type="l"), pch=20)
